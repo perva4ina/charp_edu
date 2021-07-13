428f91c50aaac65d21b39beb06b2a1be2b3220c3
Oleksii Podorozhko <oleksii.podorozhko@dataart.com>
2021-07-13 12:02:54 +0300

    initial commit

---
+++ csharp_edu/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace csharp_edu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region console
+            
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
+            
+            #endregion
+
+            #region data types
+            
+            bool canVote = true;
+            
+            char grade = 'A';
+
+            int maxInt = int.MaxValue;
+
+            long maxLong = long.MaxValue;
+            
+            decimal maxDec = decimal.MaxValue;
+
+            float maxFloat = float.MaxValue;
+
+            double maxDouble = double.MaxValue;
+
+            Console.WriteLine("Big number " + maxDec);
+
+            var another_name = "Tom";
+            
+            Console.WriteLine("name is a {0}", another_name.GetTypeCode());
+            
+            #endregion
+            
+            #region math
+            
+            //like Java
+            
+            #endregion
+        }
+    }
+}
